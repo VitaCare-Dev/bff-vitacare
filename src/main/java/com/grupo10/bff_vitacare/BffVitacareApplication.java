d671f77dad78df9b86ef6be54c1f9bd5a8f11e13
BriamCarrasco <briamcarrasco.f@gmail.com>
2026-06-30 17:35:14 -0400

    chore: configuración y estructura inicial de proyecto

---
+++ src/main/java/com/grupo10/bff_vitacare/BffVitacareApplication.java
@@ -0,0 +1,13 @@
+package com.grupo10.bff_vitacare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffVitacareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffVitacareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo10/bff_vitacare/BffVitacareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo10.bff_vitacare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffVitacareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
